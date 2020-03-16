138cf1b0bb6bccd7ccfe043e1ae1cc7205c61d54
Safi <gettoworksafi@gmail.com>
2020-03-16 13:13:01 +0500

    Backend Integrated

--- src/components/TabContentDevices.js
+++ src/components/TabContentDevices.js
@@ -3,22 +3,80 @@ import  TabAddDevices from './tabs_nav/TabAddDevices'
 import TabSingleDevice from './TabSingleDevice'
 
 import '../css/TabsNav.css'
+import {getAllChannels,addNewDevice} from './Utils/Utils.js'
 // import styled from 'styled-components'
-const channelList = [
-    { value: 'chocolate', label: 'Chocolate' },
-    { value: 'strawberry', label: 'Strawberry' },
-    { value: 'vanilla', label: 'Vanilla' },
-];
+
 
 
 export default class TabContentDevices extends Component{
     state = {
         selectedOption: null,
+        channelList: [],
+        name:"",
+        key:"",
+        location:""
+
     };
+
+
+    componentDidMount(){
+
+        getAllChannels().then(channels=>{
+
+            console.log(channels.data)
+
+            this.setState({channelList:channels.data})
+
+
+        })
+
+
+    }
+
     handleChange = selectedOption => {
         this.setState({ selectedOption });
         console.log(`Option selected:`, selectedOption);
     };
+
+    setName = (e) => {
+
+        this.setState({name: e.target.value})
+
+    }
+
+    setKey = (e) => {
+
+        this.setState({key: e.target.value})
+
+    }
+
+    setLocation = (e) => {
+
+        this.setState({location: e.target.value})
+
+    }
+
+    RegisterNewDevice = () => {
+
+        let data ={};
+
+        if(this.state.name ==="" && this.state.key==="" && this.state.location===""){
+
+        }
+
+        else{
+
+            data = {name:this.state.name, key:this.state.key, location:this.state.location}
+            addNewDevice(data)
+
+
+        }
+
+
+    }
+
+    
+
     render() {
         const { selectedOption } = this.state;
         return (
@@ -28,49 +86,75 @@ export default class TabContentDevices extends Component{
                 <hr/>
                 <div className="row">
                     <div className="col-lg-4">
-                        <TabSingleDevice/>
+                        <div className="card cardsDevicesList">
+                            <div className="card-body">
+
+                                <h6 className="card-subtitle mb-2 text-muted">Name of device</h6>                               
+                                <div className="dropdown">
+                                    <button className="btn btn-secondary dropdown-toggle btn-sm" 
+                                    type="button" id="dropdownMenuButton" 
+                                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        Select Channel
+                                    </button>
+                                    <span className="dot"></span>
+
+                                    <span>
+                                        <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                            <a className="dropdown-item" href="#">Channel 1</a>
+                                            <a className="dropdown-item" href="#">Channel 2</a>
+                                            <a className="dropdown-item" href="#">Channel 3</a>
+                                        </div>
+
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <button type="button" className="btn btn-primary addNewDeviceButton" data-toggle="modal" data-target="#add_new_device">
+                    + Add new
+                    </button>
+
+
+                    <div className="modal fade" id="add_new_device" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div className="modal-dialog" role="document">
+                        <div className="modal-content">
+                        <div className="modal-header">
+                            <h4 className="modal-title newDeviceModal" id="exampleModalLabel">New device</h4>
+                            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div className="modal-body">
+                            <form>
+                                <div className="form-group">
+                                    <label>Key</label>
+                                    <input type="text" className="form-control" placeholder="Enter key" onChange={this.setKey.bind(this)}/>
+                                </div>
+                                <div className="form-group">
+                                    <label>Name</label>
+                                    <input type="text" className="form-control" placeholder="Enter device name" onChange={this.setName.bind(this)}/>
+                                </div>
+                                <div className="form-group">
+                                    <label>Location</label>
+                                    <input type="text" className="form-control" placeholder="Location address" onChange={this.setLocation.bind(this)}/>
+                                </div>
+                                <div className="modal-footer">
+                                    <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                                    <button type="button" className="btn btn-primary" onClick={this.RegisterNewDevice.bind(this)}>Save changes</button>
+                                </div>
+                            </form>
+                        </div>
+
+                        </div>
+                    </div>
                     </div>
                 </div>
 
-                <button type="button" className="btn btn-primary addNewDeviceButton" data-toggle="modal" data-target="#add_new_device">
-                + Add new
-                </button>
 
-
-                <div className="modal fade" id="add_new_device" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div className="modal-dialog" role="document">
-                    <div className="modal-content">
-                    <div className="modal-header">
-                        <h4 className="modal-title newDeviceModal" id="exampleModalLabel">New device</h4>
-                        <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div className="modal-body">
-                        <form>
-                            <div className="form-group">
-                                <label>Key</label>
-                                <input type="text" className="form-control" placeholder="Enter key" />
-                            </div>
-                            <div className="form-group">
-                                <label>Name</label>
-                                <input type="text" className="form-control" placeholder="Enter device name" />
-                            </div>
-                            <div className="form-group">
-                                <label>Location</label>
-                                <input type="text" className="form-control" placeholder="Location address" />
-                            </div>
-                            <div className="modal-footer">
-                                <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-                                <button type="button" className="btn btn-primary">Save changes</button>
-                            </div>
-                        </form>
-                    </div>
-
-                    </div>
-                </div>
-                </div>
+                
             </div>
+
           
                 );
     }
--- src/components/TabContentDevicesDetail.js
+++ src/components/TabContentDevicesDetail.js
@@ -29,57 +29,64 @@ export default class TabContentDevices extends Component{
     render() {
         const { selectedOption } = this.state;
     return (
-        <div className="container formForAddingDevices">
-            <TabDevicesDetail></TabDevicesDetail>
-            <hr/>
-            <div className="row">
-                <div className=" col-lg-8">
-                    <div className="tab-content">
-                        <h1><center><i>Device Details</i></center></h1>
-                        
-                        {/* start forms here for new device */}
-                        <form>
-                            <div className="form-group">
-                                <label>Device name</label>
-                                <input type="text" className="form-control">devicesdata.name</input>
-                            </div>
 
-                            <label>Channel name</label>
-                            <div className="form-group"> 
-                                <Select 
-                                value={selectedOption}
-                                onChange={this.handleChange}
-                                options={channelList}
-                                isMulti= {false}
-                                placeholder = {'Select Channel'}
-                                />
-                            </div>
-                            <div className="form-group">
-                                <label>Location name</label>
-                                <input type="text" className="form-control" placeholder="Enter location" >devicesdata.location</input>
-                            </div>
-                            <div className="form-group">
-                                <label>Key</label>
-                                <input type="text" className="form-control" placeholder="key will come here" readOnly>devicesdata.key</input>
-                            </div>
-                            
-                            <button type="submit" className="btn btn-primary submitButtonAddingDevices">Save</button>
-                            </form>
-                            
-                        {/* ends here */}
+        <div className="tabcontent">
 
+
+
+            <div className="container formForAddingDevices">
+                <TabDevicesDetail></TabDevicesDetail>
+                <hr/>
+                <div className="row">
+                    <div className=" col-lg-8">
+                        <div className="tab-content">
+                            <h1><center><i>Device Details</i></center></h1>
+                            
+                            {/* start forms here for new device */}
+                            <form>
+                                <div className="form-group">
+                                    <label>Device name</label>
+                                    <input type="text" className="form-control">devicesdata.name</input>
+                                </div>
+
+                                <label>Channel name</label>
+                                <div className="form-group"> 
+                                    <Select 
+                                    value={selectedOption}
+                                    onChange={this.handleChange}
+                                    options={channelList}
+                                    isMulti= {false}
+                                    placeholder = {'Select Channel'}
+                                    />
+                                </div>
+                                <div className="form-group">
+                                    <label>Location name</label>
+                                    <input type="text" className="form-control" placeholder="Enter location" >devicesdata.location</input>
+                                </div>
+                                <div className="form-group">
+                                    <label>Key</label>
+                                    <input type="text" className="form-control" placeholder="key will come here" readOnly>devicesdata.key</input>
+                                </div>
+                                
+                                <button type="submit" className="btn btn-primary submitButtonAddingDevices">Save</button>
+                                </form>
+                                
+                            {/* ends here */}
+
+                        </div>
                     </div>
-                </div>
-                <div className= "screenshotImage col-lg-4">
-                    <div className="card">
-                        <img className="card-img-top" 
-                        src="
-                        https://dad.neocities.org/resources/dog7.jpg
-                        " />
-                    </div> 
-                </div>
-                </div>
+                    <div className= "screenshotImage col-lg-4">
+                        <div className="card">
+                            <img className="card-img-top" 
+                            src="
+                            https://dad.neocities.org/resources/dog7.jpg
+                            " />
+                        </div> 
+                    </div>
+                    </div>
+            </div>
         </div>
+
     );
     }
 }
