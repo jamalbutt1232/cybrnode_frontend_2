7e6fcda6550187fcf6916290543a347866a3578c
Jamal <jamalbutt1232@gmail.com>
2020-03-19 19:59:12 +0500

    SingleChannel file added

---
+++ src/components/TabSingleChannel.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function TabSingleChannel() {
+    return (
+        <div>
+            <div className="card cardsDevicesList">
+                <div className="card-body">
+
+                    <h6 className="card-subtitle mb-2 text-muted">Name of the Channel</h6>                               
+                    
+                </div>
+        </div>
+            
+        </div>
+    )
+}
