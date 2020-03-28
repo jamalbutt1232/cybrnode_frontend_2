3567c4915b0ee985335c9eb63b3c4299517cb8d7
Safi <gettoworksafi@gmail.com>
2020-03-29 00:04:18 +0500

    Build correct v0.0.1

---
+++ build/precache-manifest.f287b3e9958a1d5853cec3027ffb0888.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ece36ef34cee1aa7283df59627489122",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b59d808acc88f4bb77ed",
+    "url": "/static/css/2.15d027ae.chunk.css"
+  },
+  {
+    "revision": "4020b962c7362d791976",
+    "url": "/static/css/main.40dbc891.chunk.css"
+  },
+  {
+    "revision": "b59d808acc88f4bb77ed",
+    "url": "/static/js/2.f03892d5.chunk.js"
+  },
+  {
+    "revision": "4a8dfac716b034678a092e265302119b",
+    "url": "/static/js/2.f03892d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4020b962c7362d791976",
+    "url": "/static/js/main.d7f18aae.chunk.js"
+  },
+  {
+    "revision": "e0dd112486a4ea1eb4f5",
+    "url": "/static/js/runtime-main.b02b323a.js"
+  },
+  {
+    "revision": "c283d22b2c67738d8a685f78a99d0abf",
+    "url": "/static/media/cybrnode-logo.c283d22b.png"
+  }
+]);
